8cc196747efa159f78bb1eac51bbca1e2948c1a7
Diego Moreira Alves <diegomoreiraalves.92@gmail.com>
2021-09-19 22:23:58 -0300

    [0007] Adição de funcionalidade de troca de tema claro/escuro
    - Adição da biblioteca @svgr/webpack para trabalhar com svg como se os
      mesmos fossem componentes react.
    
      Foi seguido [esse artigo](https://frontend-digest.com/how-to-import-svgs-into-nextjs-8ec6100e613f) para se executar esse processo.
    
    - Adição de botão para alternar entre o tema claro e escuro
    
    - Definindo o tema escuro como padrão ao acessar o site

--- src/components/SiteBackground/index.js
+++ src/components/SiteBackground/index.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 const SiteBackground = styled.div`
   background-color: ${({ theme }) => theme.colors.secondary};
+  transition: 0.3s;
 `;
 
 export default SiteBackground;
